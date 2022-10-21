bbe7805ca7659193b10f337f61f006e3652dfd8d
MrParkerZ7 <neverzero9@gmail.com>
2022-10-21 17:40:27 +0700

    [migrate]

--- 1_Basic/src/send.js
+++ 1_Basic/src/send.js
@@ -3,7 +3,7 @@ const connection = await connect("amqp://localhost");
 const channel = await connection.createChannel();
 
 const queue = "message";
-const message = "**demo message**";
+const message = `**demo message** ${Date().toString()}`;
 
 await channel.assertQueue(queue, { durable: true });
 channel.sendToQueue(queue, Buffer.from(message));
